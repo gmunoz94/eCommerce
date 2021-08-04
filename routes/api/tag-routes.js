a9ca80057bfee99d92c5ebf1186e41bd59bd4e9e
gmunoz94 <g.munozii@icloud.com>
2021-08-04 14:02:20 -0500

    pushing final change

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -27,6 +27,8 @@ router.get('/:id', async (req, res) => {
     if (!tagData) {
       res.status(404).json({ message: 'No tag with that ID found' })
     }
+
+    res.status(200).json(tagData)
   } catch (err) {
     res.status(500).json(err)
   }
